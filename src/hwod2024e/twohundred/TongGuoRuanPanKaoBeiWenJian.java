7a3d3d3b32f52a85312bc22790b820bf5f6a09bc
guzhongqing <2570174964@qq.com>
2024-11-28 11:19:11 +0800

    feat: 通过软盘拷贝文件

---
+++ src/hwod2024e/twohundred/TongGuoRuanPanKaoBeiWenJian.java
@@ -0,0 +1,48 @@
+package hwod2024e.twohundred;
+
+
+import java.util.Scanner;
+
+/**
+ * 题目名称：通过软盘拷贝文件
+ * 题目类型：动态规划/背包DP/01背包
+ * 题目地址：https://hydro.ac/d/HWOD2023/p/OD139
+ */
+
+
+/*
+输入样例:
+3
+737270
+737272
+737288
+
+
+输出样例:
+1474542
+
+
+
+**/
+public class TongGuoRuanPanKaoBeiWenJian {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // 文件大小
+        int[] values = new int[n];
+        // 文件占用的块数
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+
+        }
+
+
+    }
+
+
+}
+
